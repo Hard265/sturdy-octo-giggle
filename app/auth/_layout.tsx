4ad81d567af78dbcdeaecb385ecd0c4f844eadfd
Hardy Namakhwa <66388667+Hard265@users.noreply.github.com>
2024-01-18 09:47:33 +0200

    Update _layout.tsx

--- app/auth/_layout.tsx
+++ app/auth/_layout.tsx
@@ -1,6 +1,28 @@
-import { Slot } from "expo-router";
+import { Stack } from "expo-router";
 
 
 export default function Layout() {
-    return <Slot/>
+    return <Stack>
+        <Stack.Screen
+        name="setup"
+        options={{
+          // Hide the header for all other routes.
+          headerShown: false,
+        }}
+      />
+        <Stack.Screen
+        name="recover"
+        options={{
+          // Hide the header for all other routes.
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="mnemonic-confirm-modal"
+        options={{
+          // Set the presentation mode to modal for our modal route.
+          presentation: 'modal',
+        }}
+      />
+    </Stack>
 }
