b82c7cb6dd222761e259f2d0ac1346cd35b4240a
Hardy Namakhwa <66388667+Hard265@users.noreply.github.com>
2024-01-18 12:59:47 +0000

    update

--- app/chat/[address].tsx
+++ app/chat/[address].tsx
@@ -1,15 +1,84 @@
+import { Stack, useLocalSearchParams } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { FlatList, Pressable, Text, TextInput, View } from "react-native";
+import { FontAwesome5, MaterialIcons } from "@expo/vector-icons";
+import { useColorScheme } from "nativewind";
 
-import { Stack, useLocalSearchParams } from 'expo-router';
+type messageProps = {
+  id: string;
+  sender: string;
+  message: string;
+};
 
-import { Text, View } from '../../components/Themed';
+type messageRendererProps = {
+  item: messageProps;
+};
+
+const messages: messageProps[] = [
+  {
+    id: "hajs",
+    sender: "1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2",
+    message: "Hey",
+  },
+  {
+    id: "hajxs",
+    sender: "1BvBMSEYstWetqTFn5Au4m4GFg7xJJKVN2",
+    message: "How are you doing?",
+  },
+];
 
 export default function Page() {
-    const { address } = useLocalSearchParams();
+  const { address } = useLocalSearchParams();
+
+  const messageRenderer = ({ item }: messageRendererProps) => {
+    const style =
+      item.sender == address
+        ? "py-2 px-3.5 self-end shrink-1 m-1.5 border border-gray-200 bg-gray-100 rounded-l-xl rounded-br-xl rounded-tr-sm dark:bg-gray-900 dark:border-gray-800"
+        : "py-2 px-3.5 self-start shrink-1 m-1.5 border border-gray-200 bg-gray-100 rounded-r-xl rounded-bl-xl rounded-tl-sm dark:bg-gray-700 dark:border-gray-600";
 
     return (
-        <View>
-            <Stack.Screen options={{ headerShown: false, title: address.toString() }} />
-            <Text>Chat ID: {address}</Text>;
-        </View>
-    )
+      <View className={style}>
+        <Text className="text-medium font-normal py-2.5 text-gray-900 dark:text-white">
+          {item.message}
+        </Text>
+      </View>
+    );
+  };
+
+  return (
+    <View className="flex flex-col flex-1 items-center dark:bg-black">
+      <Stack.Screen
+        options={{
+          title: address.toString(),
+          headerTintColor: useColorScheme().colorScheme == "dark" ? "white" : "black",
+          headerStyle: {
+            backgroundColor:
+              useColorScheme().colorScheme == "dark" ? "black" : "white",
+          },
+        }}
+      />
+      <FlatList
+        className="flex flex-1 w-full border-t border-gray-300 dark:border-gray-800"
+        data={messages}
+        renderItem={messageRenderer}
+        keyExtractor={(item) => item.id}
+      />
+      <View className="flex-1"></View>
+      <View className="flex flex-row gap-x-3 items-center mt-auto px-2.5 py-1.5 w-full border-t border-gray-300 shadow  dark:border-gray-900">
+        <TextInput
+          multiline
+          placeholder="Aa.."
+          placeholderTextColor={"gray"}
+          className="flex-1 p-2.5 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-gray-500 dark:bg-gray-800 dark:border-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-600"
+        />
+        <Pressable className="self-end p-2">
+          <MaterialIcons
+            name="arrow-upward"
+            size={24}
+            color="white"
+          />
+        </Pressable>
+      </View>
+    </View>
+  );
 }
--- app/chat/index.tsx
+++ app/chat/index.tsx
@@ -1,13 +1,52 @@
-import { Stack } from "expo-router";
-import { View } from "react-native";
+import { Stack, router } from "expo-router";
+import { useColorScheme } from "nativewind";
+import { Pressable, View, Text, FlatList } from "react-native";
+
+type userProps = {
+  address: string;
+};
+
+type userRendererProps = {
+  item: userProps;
+};
+
+const users: userProps[] = [
+  {
+    address: "1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2",
+  },
+];
 
 export default function Page() {
+  const userRenderer = ({ item }: userRendererProps) => {
+    return (
+      <Pressable
+        className="p-2"
+        onPress={() => router.push("/chat/1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2")}
+      >
+        <Text className="text-base font-semibold text-gray-900 dark:text-white">{item.address}</Text>
+      </Pressable>
+    );
+  };
+
   return (
-    <View className="flex-1 dark:bg-black">
-      <Stack.Screen options={{
-        headerTitle: 'Chats',
-        headerTitleAlign: "center",
-      }} />
+    <View className="flex-1 items-center justify-center dark:bg-black">
+     <Stack.Screen
+        options={{
+          title:"Chats",
+          headerTitleAlign: "left",
+          headerTintColor: useColorScheme().colorScheme == "dark" ? "white" : "black",
+          headerStyle: {
+            backgroundColor:
+              useColorScheme().colorScheme == "dark" ? "black" : "white",
+          },
+        }}
+      />
+      <FlatList
+        className="flex flex-1 w-full border-t border-gray-300 dark:border-gray-800"
+        data={users}
+        renderItem={userRenderer}
+        keyExtractor={(item) => item.address}
+      />
     </View>
   );
 }
